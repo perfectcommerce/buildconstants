c3a53cd6fd098dab565502b1829909b4638940c5
Matt Singletary <matt.singletary@gmail.com>
2016-05-17 16:33:36 -0400

    Read commands from file
    Execute commands in file.  Each line is a variable with a command,
    split on '='. Environment variables are expanded appropriately
    
    TODO: should output to stdout if no file specified?
    
    TODO: variable RHS expansion could be smarter

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"flag"
 	"go/format"
@@ -33,6 +34,34 @@ type cmd struct {
 	echo bool // get value from env
 }
 
+// read commands from a text file.  Each command has its own line
+//   and the lines are of the form `VAR = echo $FOO`
+//   where $VAR is the env variable and everything after '=' will be evaluated for the value
+func cmdRead(cmdfile string) ([]cmd, error) {
+
+	f, err := os.Open(cmdfile)
+	if err != nil {
+		return []cmd{}, err
+	}
+	defer f.Close()
+	var cmds []cmd
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		splits := strings.Split(scanner.Text(), "=")
+		if len(splits) < 2 {
+			continue
+		}
+		line := strings.TrimSpace(strings.Join(splits[1:], " "))
+		c := cmd{
+			Var:  strings.TrimSpace(splits[0]),
+			Line: line,
+			echo: strings.HasPrefix(line, "$"),
+		}
+		cmds = append(cmds, c)
+	}
+	return cmds, nil
+}
+
 // operates on inputs since it needs to be in the same order
 func envTemplate(ins []cmd) string {
 	var s string
@@ -46,7 +75,7 @@ func do(command cmd) (string, error) {
 	expanded := os.Expand(command.Line, os.Getenv)
 	var out string
 	if command.echo {
-		out = os.Getenv(command.Var)
+		out = expanded
 	} else {
 		split := strings.Split(expanded, " ")
 		cmd := exec.Command(split[0], split[1:]...)
@@ -77,20 +106,28 @@ func pkg() string {
 
 func main() {
 
+	cmdfile := flag.String("i", "commands.txt", "output file")
 	fname := flag.String("o", defaultfilename, "output file")
 	packageName := flag.String("package", pkg(), "package the generated file will be in.")
 	flag.Parse()
 
-	incmds := []cmd{
-		cmd{Var: "GITVERSION", Line: "git rev-list --tags --max-count=1"},
-		cmd{Var: "GITTAG", Line: "git describe --always --tags ${GITVERSION}"},
-		cmd{Var: "GOVERSION", Line: "go version"},
-		cmd{Var: "BUILD_NUMBER", echo: true},
-		cmd{Var: "BRANCH_NAME", echo: true},
+	// incmds := []cmd{
+	// 	cmd{Var: "GITVERSION", Line: "git rev-list --tags --max-count=1"},
+	// 	cmd{Var: "GITTAG", Line: "git describe --always --tags ${GITVERSION}"},
+	// 	cmd{Var: "GOVERSION", Line: "go version"},
+	// 	cmd{Var: "BUILD_NUMBER", echo: true},
+	// 	cmd{Var: "BRANCH_NAME", echo: true},
+	// }
+	incmds, err := cmdRead(*cmdfile)
+	if err != nil {
+		os.Exit(1)
 	}
 
 	outcmds := make(map[string]string, len(incmds)+1) //since we add GOPKG
 	outcmds["GOPKG"] = *packageName
+	if err != nil {
+		os.Exit(1)
+	}
 
 	for _, cmd := range incmds {
 		val, err := do(cmd)
@@ -106,7 +143,7 @@ func main() {
 	t := template.Must(template.New("templ").Parse(templ))
 
 	var buf bytes.Buffer
-	err := t.Execute(&buf, outcmds)
+	err = t.Execute(&buf, outcmds)
 	if err != nil {
 		os.Exit(1)
 	}
